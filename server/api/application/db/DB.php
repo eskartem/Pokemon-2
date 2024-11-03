19d69af6a926b1dbd677e9b2d826a305eccaefed
minili <165643105+AlenkaNet@users.noreply.github.com>
2024-11-03 20:50:59 +0400

    Расписаны методы userInfo и pokemonUpdate

---
+++ server/api/application/db/DB.php
@@ -0,0 +1,110 @@
+<?php
+
+class DB {
+    private $pdo;
+
+    function __construct() {
+        // MySQL
+        /*
+        $host = '127.0.0.1';
+        $port = '3306';
+        $user = 'root';
+        $pass = '---';
+        $db = 'nopainnogame';
+        $connect = "mysql:host=$host;port=$port;dbname=$db;charset=utf8";
+        $this->pdo = new PDO($connect, $user, $pass);
+        */
+
+        // Postgres
+        $host = 'localhost';
+        $port = '3000';
+        $user = 'postgres';
+        $pass = '---';
+        $db = 'authPrototype';
+        $connect = "pgsql:host=$host;port=$port;dbname=$db;";
+        //$this->pdo = new PDO($connect, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    }
+
+    public function __destruct() {
+        $this->pdo = null;
+    }
+
+    // выполнить запрос без возвращения данных
+    private function execute($sql, $params = []) {
+        $sth = $this->pdo->prepare($sql);
+        return $sth->execute($params);
+    }
+
+    // получение ОДНОЙ записи
+    private function query($sql, $params = []) {
+        $sth = $this->pdo->prepare($sql);
+        $sth->execute($params);
+        return $sth->fetch(PDO::FETCH_OBJ);
+    }
+
+    // получение НЕСКОЛЬКИХ записей
+    private function queryAll($sql, $params = []) {
+        $sth = $this->pdo->prepare($sql);
+        $sth->execute($params);
+        return $sth->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getUserByLogin($login) {
+        $user = new stdClass();
+        $user -> id = '123';
+        $user-> login = 'login';
+        $user ->pass = md5($login.'111');
+        $user ->name = "имя";
+        return $user;
+        //return $this->query("SELECT * FROM users WHERE login=?", [$login]);
+    }
+
+    public function getUserByToken($token) {
+        return $this->query("SELECT * FROM users WHERE token=?", [$token]);
+    }
+
+    public function updateToken($userId, $token) {
+        //$this->execute("UPDATE users SET token=? WHERE id=?", [$token, $userId]);
+    }
+
+    public function registration($login, $password, $name) {
+        $this->execute("INSERT INTO users (login,password,name) VALUES (?, ?, ?)",[$login, $hash, $name]);
+    }
+
+    public function getChatHash() {
+        return $this->query("SELECT * FROM hashes WHERE id=1");
+    }
+
+    public function updateChatHash($hash) {
+        $this->execute("UPDATE hashes SET chat_hash=? WHERE id=1", [$hash]);
+    }
+
+    public function addMessage($userId, $message) {
+        $this->execute('INSERT INTO messages (user_id, message, created) VALUES (?,?, now())', [$userId, $message]);
+    }
+
+    public function getMessages() {
+        return $this->queryAll("SELECT u.name AS author, m.message AS message,
+                                to_char(m.created, 'yyyy-mm-dd hh24:mi:ss') AS created FROM messages as m 
+                                LEFT JOIN users as u on u.id = m.user_id 
+                                ORDER BY m.created DESC"
+        );
+    }
+//////////////////////////////////////////переписать запросы правильно по бд
+public function getPokemonByIdAndUser($userId) {
+    return $this->queryAll("SELECT * FROM pokemons WHERE user_id = ?", [$userId]);
+}      
+
+public function getPokemonById($pokemonId) {
+    return $this->query("SELECT * FROM pokemons WHERE id = ?", [$pokemonId]);
+}
+
+public function updatePokemon($pokemon) {
+    $this->execute(
+        "UPDATE pokemons SET name = ?, element = ?, lvl = ?, defense = ?, attack = ?, hp = ?, skill = ? WHERE id = ?", 
+        [$pokemon->name, $pokemon->element, $pokemon->lvl, $pokemon->defense, $pokemon-> attack, $pokemon->hp, $pokemon->skill, $pokemon->id]
+    );
+}
+
+    
+}
---
+++ server/api/application/user/User.php
@@ -0,0 +1,111 @@
+<?php
+
+class User {
+    private $db = new DB();
+
+    function __construct($db) {
+        $this->db = $db;
+    }
+
+    public function getUser($token) {
+        return $this->db->getUserByToken($token);
+    }
+
+    public function login($login, $hash, $rnd) {
+        $user = $this->db->getUserByLogin($login);
+        if ($user) {
+            if (md5($user->password . $rnd) === $hash) {
+                $token = md5(rand());
+                $this->db->updateToken($user->id, $token);
+                return [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'token' => $token
+                ];
+            }
+            return ['error' => 1002];
+        }
+        return ['error' => 1005];
+    }
+
+    public function logout($token) {
+        $user = $this->db->getUserByToken($token);
+        if ($user) {
+            $this->db->updateToken($user->id, null);
+            return true;
+        }
+        return ['error' => 1003];
+    }
+
+    
+    public function registration($login, $password, $name) {
+
+        // Проверка пароля
+        if (!$this->isValidPassword($password)) {
+            return ['error' => 1007]; 
+        }
+
+        $user = $this->db->getUserByLogin($login, $password);
+        if ($user) {
+            return ['error' => 1001];
+        }
+        $this->db->registration($login, $password, $name);
+        $user = $this->db->getUserByLogin($login, $password);
+        if ($user) {
+            $token = md5(rand());
+            $this->db->updateToken($user->id, $token);
+            return [
+                'id' => $user->id,
+                'name' => $user->name,
+                'token' => $token
+            ];
+        }
+        return ['error' => 1004];
+    }
+
+    private function isValidPassword($password) {
+        // Проверка условий для пароля
+        return preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!"№;%:?*()_+=\-\`{}"?><.])(?=.*\S).{8,}$/', $password);
+    }
+
+    //Gamer Info
+
+    public function userInfo($token) {
+        $user = $this->db->getUserByToken($token);
+        if ($user) {
+            $pokemons = $this->db->getPokemonByIdAndUser($user->id);
+            return [
+                'id' => $user->id,
+                'name' => $user->name,
+                'pokemons' => $pokemons
+            ];
+        }
+        return ['error' => 404]; 
+    }
+    
+    public function pokemonUpdate($token, $pokemonId) {
+        // Получаем пользователя по токену
+        $user = $this->db->getUserByToken($token);
+        if (!$user) {
+            return ['error' => 404];
+        }
+    
+        // Проверяем, существует ли покемон для данного пользователя
+        $pokemon = $this->db->getPokemonByIdAndUser($pokemonId, $user->id);
+        if (!$pokemon) {
+            return ['error' => 2009]; // Покемон не найден
+        }
+    
+        // Обновляем информацию о покемоне
+        $updateSuccess = $this->db->updatePokemon($pokemonId);
+        if ($updateSuccess) {
+            return [
+                'pokemonId' => $pokemonId,
+                
+            ];
+        } else {
+            return ['error' => 2008]; 
+        }
+    }
+    
+}
