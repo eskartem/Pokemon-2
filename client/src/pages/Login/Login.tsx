a72f916734a8ee5eb641050209c2de2f1a30fe0d
SeREnbKa <eskartem@gmail.com>
2024-11-07 22:27:58 +0400

    Revert "Merge pull request #45 from eskartem/004-mainMenuDesigne"
    This reverts commit 9d7a6ad06ea3288cd47f0b2ca9137ab72b99d532, reversing
    changes made to 80fa82032d2c369f8c5a878b3a183152b0c3b09d.

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -2,7 +2,7 @@ import React, { useContext, useRef } from 'react';
 import { ServerContext } from '../../App';
 import Button from '../../components/Button/Button';
 import { IBasePage, PAGES } from '../PageManager';
-import LoginImg from '../../assets/img/login/login.png';
+
 import './Login.scss';
 
 const Login: React.FC<IBasePage> = (props: IBasePage) => {
@@ -15,9 +15,9 @@ const Login: React.FC<IBasePage> = (props: IBasePage) => {
         if (!loginRef.current || !passwordRef.current) {return;}
         const login = loginRef.current.value;
         const password = passwordRef.current.value;
-        //if (1) { // тестовое условие, чтобы логин всегда был успешный и работал без бекенда
+        // if (1) { // тестовое условие, чтобы логин всегда был успешный и работал без бекенда
         if (login && password && await server.login(login, password)) { // login: admin, пароль: 111
-            setPage(PAGES.MAINMENU);
+            setPage(PAGES.MAINMENU); //PAGES.MAINMENU
         }
     }
 
@@ -25,21 +25,7 @@ const Login: React.FC<IBasePage> = (props: IBasePage) => {
     const backClickHandler = () => setPage(PAGES.PRELOADER);
 
     return (<div className='login'>
-        <div className='login-wrapper'>
-            <div className='login-inputs'>
-                <input ref={loginRef} placeholder='логин' />
-                <input ref={passwordRef} placeholder='пароль' type='password' />
-            </div>
-            <div className='login-buttons'>
-                <img className='loginClick' src={LoginImg} alt='' onClick={loginClickHandler}></img>
-            </div>
-            <span className='register-link' onClick={registerClickHandler}>
-                У меня нет аккаунта
-            </span>
-        </div>
-    </div>)
-
-    /*return (<div className='login'>
+        <div>Логин</div>
         <div className='login-wrapper'>
             <div className='login-inputs'>
                 <input ref={loginRef} placeholder='логин' />
@@ -53,7 +39,7 @@ const Login: React.FC<IBasePage> = (props: IBasePage) => {
                 У меня нет аккаунта
             </span>
         </div>
-    </div>)*/
+    </div>)
 }
 
 export default Login;
