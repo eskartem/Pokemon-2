bafa644ee3d3ea17391a4676f7812c077d652ca0
kap1984 <eeonesham@gmail.com>
2024-11-13 01:35:20 +0400

    dd

--- server/api/application/map/Map.php
+++ server/api/application/map/Map.php
@@ -11,12 +11,17 @@ class Map {
         return true; // просто заглушка
     }
 
-    public function getMap($token){
+    public function getMap($token) {
         $user = $this->db->getUserByToken($token);
         if ($user) {
-            return $map = $this->db->getMap($token);
+            $mapData = $this->db->getMap($token);
             //хз, имеет ли вообще смысл писать этот кал, просто хочется чтобы к бд обращались только внутри класса DB
+            if ($mapData) {
+                return $mapData;
+            } else {
+                return ['error' => 9000, 'message' => 'Карта не найдена.'];
+            }
         }
-        return ['error' => 9000]
+        return ['error' => 9000, 'message' => 'Зарегестрируйтесь или войдите для просмотра карты.'];
     }
 }
