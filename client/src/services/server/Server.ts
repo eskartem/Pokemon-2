d33445ceef5f2e3d68451290c76092bd80bd43df
Snlcma12 <daniil_semenov562@mail.ru>
2024-11-29 20:24:42 +0300

    buy server.ts

--- client/src/services/server/Server.ts
+++ client/src/services/server/Server.ts
@@ -122,7 +122,7 @@ class Server {
     }
 
     async buyItem(id: string): Promise<boolean | null> {
-        const result = await this.request<boolean>('buyItem', { id });
+        const result = await this.request<boolean>('buy', { id });
         return result;
     }
     
