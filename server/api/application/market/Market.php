a01a553c7c92cb29c927d1e4f994544876cfde10
kap1984 <eeonesham@gmail.com>
2024-12-04 22:35:06 +0400

    правки

--- server/api/application/market/Market.php
+++ server/api/application/market/Market.php
@@ -14,13 +14,18 @@ class Market {
         return ['error', 2001];
     }
 
-    public function sell($userId, $marketType, $inventoryInfo, $lotExtra, $objectId, $resourceAmount){
-        if ($marketType === 'merchant'){
-            $sellingResource = $this->db->getResourcesById($objectId);
-            if (!$sellingResource){
-                return ['error' => 3008];
-            }
+    public function sell($userId, $marketType, $inventoryInfo, $objectId, $resourceAmount){
+        $sellingResource = $this->db->getResourcesById($objectId);
+        if (!$sellingResource){
+            return ['error' => 3008];
+        }
 
+        $merchantCosts = [
+            'crystals' => 10,
+            'shells' => 20,
+            'eggs' => $merchantCosts['shells'] * 50      //курс 50 скорлуп = 1 яйцо
+        ];
+        if ($marketType === 'merchant'){
             foreach ($inventoryInfo as $item){
                 if ($item['resource_id'] === $objectId){
                     if ($resourceAmount <= $item['resource_amount']){
@@ -30,7 +35,10 @@ class Market {
                         }
                         
                         if ($sellingResource->name === 'Осколки Яиц'){
-                            return $this->db->sellResources($sellingResource->id, $resourceAmount);
+                            return ['resDecrease' => $this->db->sellResources($sellingResource->id, $resourceAmount),
+                                    'moneyIncrease' => $this->db->changeMoney($userId, $merchantCosts['shells']*$resourceAmount).
+                                    'message' => [$merchantCosts['shells'], $resourceAmount]
+                                    ];
                         }
                         
                         if ($sellingResource->name === 'Яйцо'){
@@ -46,11 +54,6 @@ class Market {
         }
 
         if ($marketType === 'exchanger'){
-            $sellingResource = $this->db->getResourcesById($objectId);
-            if (!$sellingResource){
-                return ['error' => 3008];
-            }
-
             foreach ($inventoryInfo as $item){
                 if ($item['resource_id'] === $objectId){
                     if ($resourceAmount <= $item['resource_amount']){
@@ -65,32 +68,7 @@ class Market {
             }
         }
 
-        if ($marketType === 'lot'){
-            if ($lotExtra === 'pokemon'){
-
-            }
-
-            if ($lotExtra === 'item'){
-                $sellingResource = $this->db->getResourcesById($objectId);
-                if (!$sellingResource){
-                    return ['error' => 3008];
-                }
-                /*
-                ОЧЕНЬ ВАЖНО
-                удалить любые упоминания про покемонов и лоты тут, 
-                реализовать все в методе makeLot, 
-                перенести селлинг ресурс до ифов
-                раз селл остается только про торговца и обменник, то можно не передавать обжектАйди, а только ресурсНейм
-                значительно упрощает весь пиздец, написанный в этом методе
-                я убью себя
-                ЕСЛИ тут остается что-то про лоты, то бросать метод создания лота только для предметов в этом параметре (маловероятно)
-                апдейт лот находится внутри мейклот, создает хэш когда кто-то делает действие на рынке -> добавить в бай обновление хэшей тоже
-                или делать обновление постоянно, как чат, тогда не надо в бай
-                */
-                //$this->makeLot($objectId);
-            }
-            
-        }
+       
     }
 
 }
