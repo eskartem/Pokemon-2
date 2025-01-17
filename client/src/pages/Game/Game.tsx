4bb450c6c9904dd636fc59742759aeccd316bb61
VanekWTF <minemaster55yt@mail.ru>
2025-01-17 17:27:36 +0400

    backup

--- client/src/pages/Game/Game.tsx
+++ client/src/pages/Game/Game.tsx
@@ -2,7 +2,6 @@ import React, {useContext, useEffect, useState} from 'react';
 import Button from '../../components/Button/Button';
 import Map from '../../components/Map/Map';
 import Chat from '../../components/Chat/Chat';
-import UserPanel from '../../components/UserPanel/UserPanel';
 import { StoreContext, ServerContext } from '../../App';
 import { IBasePage, PAGES } from '../PageManager';
 import CoinImg from '../../assets/img/coin.png';
@@ -10,6 +9,7 @@ import RubyImg from '../../assets/img/Ruby.png';
 import EggImg from '../../assets/img/Egg.png';
 import EggBreakImg from '../../assets/img/EggBreak.png';
 import ExitImg from '../../assets/img/ExitImg.png';
+import VolumeImg from '../../assets/img/volume.png';
 import ChatImg from '../../assets/img/chat.png';
 import ChatCloseImg from '../../assets/img/chatclose.png';
 
@@ -41,35 +41,13 @@ const Game: React.FC<IBasePage> = (props: IBasePage) => {
     }
 
     const panelHeight = '65px';
-    //style={{ paddingTop: panelHeight }} //Для <div className="map-container">
     
     if (!user) { return ( <div><h1> Что-то пошло не так. </h1></div> );} // закоментировать для работы без бекэнда
 
-    /*return (
-        <div className="game-wrapper">
-            <UserPanel
-                user={user} // Передаем пользователя
-                inventoryClickHandler={inventoryClickHandler}
-                marketClickHandler={marketClickHandler}
-                battleClickHandler={battleClickHandler}
-                logoutClickHandler={logoutClickHandler}
-            />
-            {isChatVisible && (
-                <div className='game-chat'>
-                    <Chat />
-                </div>
-            )}
-            <img className='chat-button' onClick={toggleChatVisibility} src={isChatVisible ? ChatCloseImg : ChatImg } />
-            <div className="map-container" style={{ paddingTop: panelHeight }}>
-                <Map />
-            </div>
-        </div>
-    );*/
-
     return (
         <div className="game-wrapper">
-            <div className="header-user-panel">
-                <div className='user-panel-left-items'>
+            <div className="button-panel-test">
+                <div className='button-panel-test-left'>
                     <h1 id='test-game-h1-name' className='user-panel-nick'> {user?.name} | </h1>
                     <img src={CoinImg} alt="CoinImg" className='user-resources'/>
                     <h1 id='test-game-h1-coins' className='user-resources-coins'> {user?.coins} </h1>
@@ -77,12 +55,13 @@ const Game: React.FC<IBasePage> = (props: IBasePage) => {
                     <img src={EggImg} alt="EggImg" className='user-resources'/>
                     <img src={EggBreakImg} alt="EggBreakImg" className='user-resources'/>
                 </div>
-                <div className='user-panel-centre-items'>
+                <div className='button-panel-test-centre'>
                     <Button id='test-game-button-inventory' onClick={inventoryClickHandler} text='Инвентарь' />
                     <Button id='test-game-button-market' onClick={marketClickHandler} text='Рынок' />
                     <Button id='test-game-button-battle' onClick={battleClickHandler} text='Битва' />
                 </div>
-                <div className='user-panel-right-items'>
+                <div className='button-panel-test-right'>
+                    <img id='test-game-img-mute' className='img-volume' src={VolumeImg} alt='VolumeImg' onClick={muteButtonHandler} />
                     <img id='test-game-img-logout' className='img-logout' src={ExitImg} alt="ExitImg" onClick={logoutClickHandler} />
                 </div>
             </div>
@@ -92,7 +71,7 @@ const Game: React.FC<IBasePage> = (props: IBasePage) => {
                 </div>
             )}
             <img className='chat-button' onClick={toggleChatVisibility} src={isChatVisible ? ChatCloseImg : ChatImg } />
-            <div className="map-container">
+            <div className="map-container" style={{ paddingTop: panelHeight }}>
                 <Map />
             </div>
         </div>
